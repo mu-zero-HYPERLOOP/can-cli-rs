3f3eb9df0ce761b64f514a134393a9d7c3176fe3
kistenklaus <karlsasssie@gmail.com>
2023-12-03 17:08:55 +0100

    hints on how to invoke the functions

--- src/commands/config/show.rs
+++ src/commands/config/show.rs
@@ -4,6 +4,7 @@ use crate::local::{load_config, load_config_path};
 
 
 
+// to invoke run $cargo run -- config show
 pub fn command_config_show() {
     let config_path = load_config_path();
     let network_config : config::NetworkRef = load_config();
@@ -15,16 +16,19 @@ pub fn command_config_show() {
     println!("{network_config}");
 }
 
+// to invoke run $cargo run -- config show nodes
 pub fn command_config_show_nodes() {
     let network_config : config::NetworkRef = load_config();
     // TODO log nodes in the network_config to the console!
 }
 
+// to invoke run $cargo run -- config show messages
 pub fn command_config_show_messages() {
     let network_config : config::NetworkRef = load_config();
     // TODO log nodes in the network_config to the console!
 }
 
+// to invoke run $cargo run -- config show types
 pub fn command_config_show_types() {
     let network_config : config::NetworkRef = load_config();
     // TODO log nodes in the network_config to the console!
