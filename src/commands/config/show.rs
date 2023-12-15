4a385cb500581468318bce47c4ea4df0879d43a7
Nico <ubncr@student.kit.edu>
2023-12-15 14:20:07 +0100

    Added commands show and show_nodes

--- src/commands/config/show.rs
+++ src/commands/config/show.rs
@@ -6,20 +6,17 @@ use crate::local::{load_config, load_config_path};
 
 // to invoke run $cargo run -- config show
 pub fn command_config_show() {
-    let config_path = load_config_path();
-    let network_config : config::NetworkRef = load_config();
-    // TODO log network config to the console!
-    
-    // TODO currently the network config implements Display, which also
-    // logs the console, but maybe we can find a better way to display the data!
-    
-    println!("{network_config}");
+    let network_config: config::NetworkRef = load_config();
+    println!("Network Configuration:\n{network_config}");
 }
 
 // to invoke run $cargo run -- config show nodes
 pub fn command_config_show_nodes() {
-    let network_config : config::NetworkRef = load_config();
-    // TODO log nodes in the network_config to the console!
+    let network_config: config::NetworkRef = load_config();
+    println!("Nodes in Network:");
+    for node in network_config.nodes() { // Assuming a method `nodes` exists
+        // println!("{node}");
+    }
 }
 
 // to invoke run $cargo run -- config show messages
