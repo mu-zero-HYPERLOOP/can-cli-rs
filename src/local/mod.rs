10e9f2e86cdfbb5638e594822ac129e47d780422
Nico <ubncr@student.kit.edu>
2023-12-15 14:15:41 +0100

    Changed load_config_path

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -1,11 +1,16 @@
 use can_config_rs::config;
+use dirs;
+
 
 // this crate is responsible for accessing the local (user) appdata.
 
-
 pub fn load_config_path() -> String {
-    "test.yaml".to_owned()
+    let appdata_dir = dirs::config_dir().expect("Appdata directory not found");
+    let config_path = appdata_dir.join("my_app_config").join("config.yaml");
+
+    config_path.to_str().unwrap().to_owned()
 }
+
 
 pub fn load_config() -> config::NetworkRef {
     let path = load_config_path();
