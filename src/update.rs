2edbc334bf016b0c85833762681a41163af44c77
kistenklaus <karlsasssie@gmail.com>
2024-04-04 18:45:52 +0200

    build openssl vendorless

--- src/update.rs
+++ src/update.rs
@@ -7,6 +7,8 @@ use crate::errors::{Error, Result};
 const CANZERO_CLI_REPO: &'static str = "https://github.com/mu-zero-HYPERLOOP/can-cli-rs.git";
 const CANZERO_CLI_PATH: &'static str = "canzero-cli";
 
+const PI_ARCH : &'static str = "armv7-unknown-linux-gnueabihf";
+
 pub fn command_update() -> Result<()> {
     let Ok(rustup_target_list) = std::process::Command::new("rustup")
         .arg("target")
@@ -19,49 +21,58 @@ pub fn command_update() -> Result<()> {
     let list = std::str::from_utf8(&rustup_target_list.stdout).unwrap();
     if !list
         .split('\n')
-        .any(|t| t == "armv7-unknown-linux-gnueabihf")
+        .any(|t| t == PI_ARCH)
     {
         println!(
-            "Missing cargo target armv7-unknown-linux-gnueabihf. 
+            "Missing rust target {PI_ARCH}. 
 Required for crosscompilation to raspberry pies!
 Try installing it by running:
-$ rustup target add armv7-unknown-linux-gnueabihf"
+$ rustup target add {PI_ARCH}"
         );
     }
 
     // fetch canzero-cli repository
+    println!("Downloading {CANZERO_CLI_REPO}");
     let mut canzero_cli_path = AppData::dir();
     canzero_cli_path.push(CANZERO_CLI_PATH);
-    let canzero_cli_path = std::fs::canonicalize(canzero_cli_path).unwrap();
-    println!("{}", canzero_cli_path.to_str().unwrap());
     if canzero_cli_path.exists() {
-        let x = std::process::Command::new("cd")
-            .arg(canzero_cli_path.to_str().unwrap())
-            .output();
-        println!("{x:?}");
         std::process::Command::new("git")
             .arg("fetch")
-            .output()
+            .current_dir(&canzero_cli_path)
+            .spawn()
+            .unwrap()
+            .wait()
             .unwrap();
         std::process::Command::new("git")
             .arg("reset")
             .arg("--hard")
             .arg("origin/main")
-            .output()
+            .current_dir(&canzero_cli_path)
+            .spawn()
+            .unwrap()
+            .wait()
             .unwrap();
     } else {
         let git_clone_res = std::process::Command::new("git")
             .arg("clone")
             .arg(CANZERO_CLI_REPO)
             .arg(&canzero_cli_path)
-            .output()
+            .spawn()
+            .unwrap()
+            .wait()
             .unwrap();
-        println!("res = {git_clone_res:?}");
     }
 
-    std::process::Command::new("cd")
-        .arg(&canzero_cli_path)
-        .exec();
+    println!("Cross-Compiling {CANZERO_CLI_REPO}");
+    let x = std::process::Command::new("cross")
+        .arg("build")
+        .arg("--release")
+        .arg(&format!("--target={PI_ARCH}"))
+        .current_dir(&canzero_cli_path)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 
     Ok(())
 }
